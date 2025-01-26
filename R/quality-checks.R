6ba02d10e75f8d15f279005cfcd232912fc24e87
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-01-27 10:35:47 +1100

    add cols_constant, cols_missing and rows_missing

--- R/quality-checks.R
+++ R/quality-checks.R
@@ -35,4 +35,86 @@ is_observational_unit <- function(.data, ..., .message = TRUE) {
   invisible(res)
 }
 
+#' Identify constant columns
+#'
+#' Similar to janitor::remove_constant, except this function
+#' aims to identify the constant columns without removing them.
+#' If there are no constant columns, it will return NA.
+#'
+#' @param data The data frame
+#' @param na.rm Should missing values be removed?
+#' @return A character vector of constant columns.
+#' @export
+cols_constant <- function (data, na.rm = FALSE) {
+  if(nrow(data) <= 1) {
+    cli::cli_alert("The data has only one row.")
+    return(colnames(data))
+  }
+  cols <- sapply(X = seq_len(ncol(data)), FUN = function(idx) {
+    column_to_test <- if (is.matrix(data)) {
+      data[, idx]
+    }
+    else {
+      data[[idx]]
+    }
+    length(unique(if (na.rm) {
+      stats::na.omit(column_to_test)
+    } else {
+      column_to_test
+    })) <= 1
+  })
+  if(!any(cols)) {
+    cli::cli_alert("No constant columns.")
+    return(NA)
+  }
+  colnames(data)[cols]
+}
 
+
+#' Identify columns that have certain proportion of missing values
+#'
+#' Similar to janitor::remove_empty, except this function
+#' aims to identify the columns without removing them.
+#' If there are no identified columns, it will return NA.
+#'
+#' @param data The data frame
+#' @param cutoff The minimum cutoff for the proportion of missing values.
+#' @return A character vector of column names with missing values with at least a certain proportion.
+#' @export
+cols_missing <- function (data, cutoff = 1) {
+  cols <- if (cutoff >= 1) {
+    colSums(is.na(data)) == nrow(data)
+  }
+  else {
+    (colSums(is.na(data))/nrow(data)) >= cutoff
+  }
+  if(!any(cols)) {
+    cli::cli_alert("No identified columns with missing proportion.")
+    return(NA)
+  }
+  colnames(data)[cols]
+}
+
+#' Identify rows that have certain proportion of missing values
+#'
+#' Similar to janitor::remove_empty, except this function
+#' aims to identify the columns without removing them.
+#' If there are no identified columns, it will return NA.
+#'
+#' @param data The data frame
+#' @param cutoff The minimum cutoff for the proportion of missing values.
+#' @return An integer vector of rows with missing values with at least a certain proportion.
+#' @export
+rows_missing <- function(data, cutoff = 1) {
+  rows <- if (cutoff >= 1) {
+    rowSums(is.na(data)) == ncol(data)
+  }
+  else {
+    (rowSums(is.na(data))/ncol(data)) >= cutoff
+  }
+  if(!any(rows)) {
+    cli::cli_alert("No identified rows with missing proportion.")
+    return(NA)
+  }
+  which(rows)
+}
