bdd558bc2d00615518bc249d7559961e15f7bc56
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-04-21 12:51:39 +1000

    rename met1 -> met

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Phenotypic data
+#'
+#' A multi-environmental trial data with yield.
+#'
+#' @family met-data
+"met_pheno"
+
+
+#' Marker data
+#'
+#' The marker information for the genotypes in the multi-environmental trial.
+#'
+#' @family met-data
+"met_marker"
+
+#' Pedigree data
+#'
+#' The pedigree information for the genotypes in the multi-environmental trial.
+#'
+#' @family met-data
+"met_pedigree"
